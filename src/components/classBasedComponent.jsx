b5b145010192813d02ad20d8e5573a1ec6e03a20
coffee-and-debugging <adhikariunique56@gmail.com>
2024-02-07 19:06:41 +0545

    3rd update

--- src/components/classBasedComponent.jsx
+++ src/components/classBasedComponent.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 
 class classBasedComponent extends Component {
+
+
+    state = {
+        count: 0
+
+    }
+
+    handleClick = () => {
+        const { count } = this.state;
+
+        this.setState({
+            count: count + 1
+        }, () => {
+            console.log(this.state)
+        })
+    }
+
+
+
     render() {
+        console.log(this.state)
         return (
-            <div> its me classBasedComponent</div >
+            <div>
+                <button onClick={this.handleClick}>Click</button>
+                {
+                    this.state.count === 5 && "Count is 5"
+                }
+            </div >
         )
     }
 
